b45f0dee2545162e6b349a269c2b2165ebb94c34
AlderAutomation <mheltman@alderautomation.ca>
2023-06-19 22:25:36 -0700

    fix list devices. add getdevice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import config
 import base64
+import json
 
 
 BASE_URL = "https://api.fluent.walchem.com/"
@@ -20,23 +21,43 @@ class Fluent_Data:
     def list_devices(self) -> None: 
         type_of_req = "controller/list/"
         self.url = BASE_URL + type_of_req
+        response = requests.get(self.url, headers=HEADERS)
+        json_response = response.json()
+        pretty_response = json.dumps(json_response, indent=4)
+
+        print(pretty_response)
+
+
+    def get_device(self, serial: str) -> None:
+        type_of_req = f"controller/current-readings/{serial}"
+        self.url = BASE_URL + type_of_req
+        response = requests.get(self.url, headers=HEADERS)
+        json_response = response.json()
+        pretty_response = json.dumps(json_response, indent=4)
+
+        print(pretty_response)
+
 
 
 def main():
-    # FAPI = Fluent_Data()
+    FAPI = Fluent_Data()
     # FAPI.list_devices()
-    try:
-        response = requests.get(BASE_URL + "controller/list", headers=HEADERS)
-        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+    FAPI.get_device('2012140870')
 
-        print(response.status_code)
-        print(response.json())
 
-    except requests.exceptions.RequestException as e:
-        print("Error occurred during the request:", e)
 
-    except ValueError as e:
-        print("Error occurred while parsing the response:", e)
+    # try:
+    #     response = requests.get(BASE_URL + "controller/list", headers=HEADERS)
+    #     response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+
+    #     print(response.status_code)
+    #     print(response.json())
+
+    # except requests.exceptions.RequestException as e:
+    #     print("Error occurred during the request:", e)
+
+    # except ValueError as e:
+    #     print("Error occurred while parsing the response:", e)
 
 if __name__=="__main__":
     main()
