8449945fb24e730c1712ec7cd746b9539579277d
AlderAutomation <mheltman@alderautomation.ca>
2023-06-21 19:58:08 -0700

    refactor. add get_readings function

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import requests
-import config
-import base64
 import json
 
 
 BASE_URL = "https://api.fluent.walchem.com/"
-api_key = config.API_KEY
-API_KEY = base64.b64encode(api_key.encode("utf-8")).decode("utf-8")
 HEADERS = {
   'accept': '*/*',
   'authorization': 'Basic bWhlbHRtYW5AYWxkZXJhdXRvbWF0aW9uLmNhOmE1ODhjMDAwLWZjNWYtNGNmNy1iNDYxLTM5OWQ3ZjAyMGQ3Mw=='
@@ -28,36 +24,35 @@ class Fluent_Data:
         print(pretty_response)
 
 
-    def get_device(self, serial: str) -> None:
+    def get_device(self, serial: str) -> json:
         type_of_req = f"controller/current-readings/{serial}"
         self.url = BASE_URL + type_of_req
         response = requests.get(self.url, headers=HEADERS)
         json_response = response.json()
-        pretty_response = json.dumps(json_response, indent=4)
 
-        print(pretty_response)
+        return json_response
+
+
+    def get_readings(self, data) -> list:
+        readings = data['readings']
+        for r in readings:
+            if r['channel-number'] == "S11":
+                print(r['channel-name'])
+
+
+    def json_test_file(self) -> json:
+        with open("./test_data/2012140870.json", "r") as json_file:
+            file_data = json.load(json_file)
+        
+        return file_data
 
 
 
 def main():
     FAPI = Fluent_Data()
-    # FAPI.list_devices()
-    FAPI.get_device('2012140870')
+    # FAPI.get_readings(FAPI.get_device('1703140568'))
+    FAPI.get_readings(FAPI.json_test_file())
 
-
-
-    # try:
-    #     response = requests.get(BASE_URL + "controller/list", headers=HEADERS)
-    #     response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
-
-    #     print(response.status_code)
-    #     print(response.json())
-
-    # except requests.exceptions.RequestException as e:
-    #     print("Error occurred during the request:", e)
-
-    # except ValueError as e:
-    #     print("Error occurred while parsing the response:", e)
 
 if __name__=="__main__":
     main()
