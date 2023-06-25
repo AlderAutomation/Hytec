74397cdc1568f104b48487db7cf1256100bbf94d
AlderAutomation <mheltman@alderautomation.ca>
2023-06-25 15:06:02 -0700

    refactor into class. add devlookup funct

--- sql.py
+++ sql.py
@@ -4,16 +4,23 @@ import mysql.connector
 
 class Hysql:
     def __init__(self) -> None:
-        self.mydb = mysql.connector.connect(
+        self.my_db = mysql.connector.connect(
             host=config.SQLIP,
             port=config.PORT,
             user=config.SQLUSER,
             password=config.SQLPWD
         )
 
+        self.my_cursor = self.my_db.cursor()
+
         
     def device_lookup(self, serial:str) -> None: 
-        pass 
+        '''Function to lookup device data based on serial number'''
+
+        query = f"SELECT * from myhytec_dotcomdb.oi4h8_installations where device_serial_num = '{serial}';"
+        self.my_cursor.execute(query)
+
+        return self.my_cursor.fetchall()
 
 
     def installations_data_add_row(self, dataclass:object) -> None:
@@ -22,7 +29,7 @@ class Hysql:
 
 def main():
     hysql = Hysql()
-    print(hysql.mydb)
+    print(hysql.device_lookup(1609132084))
 
 
 if __name__=="__main__":
