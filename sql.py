9a339ebeefebe4e41d8f86718beaffa8760b75bf
AlderAutomation <mheltman@alderautomation.ca>
2023-06-25 20:27:37 -0700

    add db insert

--- sql.py
+++ sql.py
@@ -27,23 +27,31 @@ class Hysql:
         return self.my_cursor.fetchall()
 
 
-    def installations_data_add_row(self, dataclass:object) -> None:
-        print(dataclass)
-        # TODO Write to SQL 
-        # TODO include dataclass set_posted_time 
+    def installations_data_add_row(self, dataclass: object) -> None:
+        dataclass.set_posted(self.posted)
 
+        table_name = "myhytec_dotcomdb.oi4h8_installation_data"
+        cols = ['installations_installation_id', 'received_datetime', 'hardware_name', 'custom_name', 'units', 'data_value', 'posted']
+        values = [dataclass.installation_id, dataclass.received_datetime, dataclass.hardware_name, dataclass.custom_name, dataclass.units, dataclass.data_value, dataclass.posted]
+        
+        columns_str = ', '.join(cols)
+        placeholders = ', '.join(['%s'] * len(values)) 
+
+        query = f"INSERT INTO {table_name} ({columns_str}) VALUES ({placeholders});"
+
+        self.my_cursor.execute(query, values)
+        self.my_db.commit()
+        print(self.my_cursor.rowcount, 'records inserted.')
 
 
 def main():
     hysql = Hysql()
-    # read_obj = reading.Readings('1705301238', 'AquaSoft', 'S11', 'C_Cond', 'Value', '449', 'ppm')
+    read_obj = reading.Readings('1705301238', 'AquaSoft', 'S11', 'C_Cond', 'Value', '449', 'ppm')
 
-    # install_id = hysql.device_lookup(read_obj.dev_serial)
-    # read_obj.set_install_id(install_id[0][0])
+    install_id = hysql.device_lookup(read_obj.dev_serial)
+    read_obj.set_install_id(install_id[0][0])
 
-    # hysql.installations_data_add_row(read_obj)
-
-    print(hysql.posted)
+    hysql.installations_data_add_row(read_obj)
 
 
 if __name__=="__main__":
