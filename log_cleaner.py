bf1e0204934d04e5e88241e49a39dca92572667e
Matthew <mheltman@alderautomation.ca>
2023-08-29 14:58:31 -0700

    add log cleanup class base

---
+++ log_cleaner.py
@@ -0,0 +1,17 @@
+import os 
+import datetime
+
+class Log_Cleaner ():
+    def __init__(self) -> None:
+        pass
+
+    def remove_older_logs():
+        pass
+
+
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()
